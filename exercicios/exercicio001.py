27f1dc4b457deadebd2456e58ce26e532f30bbc2
ISJ.AngelBlack <50343536+ISJ-BlackAngel@users.noreply.github.com>
2023-10-30 14:02:15 -0300

    Update exercicio001.py

--- exercicios/exercicio001.py
+++ exercicios/exercicio001.py
@@ -1,1 +1,7 @@
+# nesse exercicio foi para colocar em pratica a declaração de variavel, e o uso pratico do input
+kilometros_percorridos = input('DIGITE QUANTOS KM/H JÁ PERCORREU\n>>> ')
+combustivel_gasto = input('DIGITE QUANTOS COMBUSTIVEL FOI GASTO\n>>> ')
 
+media_calculada = int(kilometros_percorridos) / int(combustivel_gasto)
+
+print(f'o consumo medio foi {media_calculada:^.2f}')
