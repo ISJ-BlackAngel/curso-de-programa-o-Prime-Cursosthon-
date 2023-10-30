428d00c016bd9e180cc8b33a0ee89cd4ddf04094
ISJ.AngelBlack <50343536+ISJ-BlackAngel@users.noreply.github.com>
2023-10-30 15:36:56 -0300

    Create exercicio011.py

---
+++ exercicios/exercicio011.py
@@ -0,0 +1,9 @@
+"""Faça um algoritmo para ler uma matriz de 3x4 de numeros reais e depois exibir o elemento do canto superior esquerdo e do canto inferior direito"""
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0, 6, 8],
+]
+print('canto superior esquerdo:', matrix[0][0])
+print('Canto inferior direito:', matrix[3][2])
